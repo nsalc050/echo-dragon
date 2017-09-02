9ee63a3d5b845d8953b370c3289f495becb7bd43
nsalc050 <31559354+nsalc050@users.noreply.github.com>
2017-09-02 15:31:05 -0400

    Create binarynumber.js

---
+++ binarynumber.js
@@ -0,0 +1,17 @@
+function binarynumber(a){
+
+  while(a!=1)
+  {
+    var hold = a % 2
+    if (hold == 0 )
+    {
+       document.write(hold)
+    }
+
+    if(hold == 1)
+    {
+    document.write(hold)
+    }
+    }
+    a = a /2 ;
+ }
