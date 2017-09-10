1c51fdabb9615560033573a9bce5751b29320b7b
nsalc050 <nssalnic@gmail.com>
2017-09-09 20:52:10 -0400

    hello

---
+++ serverStart.js
@@ -0,0 +1,9 @@
+var http = require('http');
+
+http.createServer(function(req,res) {
+    res.write(200,{'Content-Type':'text/plain'});
+    res.end('Hello\n');
+
+}).listen(8000);
+
+console.log('server is running');
