0dd98c511ab8aacdc01186b6099858eb6c9e1eb2
nsalc050 <nssalnic@gmail.com>
2017-09-09 20:58:31 -0400

    hello

--- serverStart.js
+++ serverStart.js
@@ -4,6 +4,8 @@ http.createServer(function(req,res) {
     res.write(200,{'Content-Type':'text/plain'});
     res.end('Hello\n');
 
-}).listen(4000);
+});
+http.listen(8000);
+
 
 console.log('server is running');
