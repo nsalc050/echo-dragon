50e3820f7cda00fadfe9f622dd7e7838fbcccc13
nsalc050 <nssalnic@gmail.com>
2017-09-14 19:01:08 -0400

    fixed function

--- serverStart.js
+++ serverStart.js
@@ -1,5 +1,5 @@
 var http = require('http');
-var Person = JSON(JSON.stringify);
+var Person = JSON.parse(JSON.stringify);
  var server = http.createServer(function(req,res) {
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.write('hello');
