b165b08a0cd0a902e1616c607731e6c3bf2cb8c6
nsalc050 <nssalnic@gmail.com>
2017-09-14 18:54:03 -0400

    new file added

--- serverStart.js
+++ serverStart.js
@@ -1,8 +1,9 @@
 var http = require('http');
-
+var Person = JSON(JSON.stringify);
  var server = http.createServer(function(req,res) {
     res.writeHead(200,{'Content-Type':'text/plain'});
     res.write('hello');
+    res.write(Person);
     res.end();
 
 });
