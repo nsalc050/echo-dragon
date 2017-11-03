ef0a2257d56819d72182b3c44c7a71a66a630fd6
nsalc050 <nssalnic@gmail.com>
2017-11-02 21:09:43 -0400

    new files

--- file.js
+++ file.js
@@ -1,15 +1,8 @@
-var json = (function () {
-    var json = null;
-    $.ajax({
-        'async': false,
-        'global': false,
-        'url': my_url,
-        'dataType': "json",
-        'success': function (data) {
-            json = data;
-            
-        }
-    }); 
-    return json;
-})(); 
-console.log(json);
+function test (){
+
+ $.getJSON('dataA.json', function (data){
+    console.log(data);
+ });
+
+  
+}
