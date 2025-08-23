83028696bfe7a2f6491674a803755d0c04345b95
Nicolas Salcedo <nssalnic@gmail.com>
2025-08-22 21:32:28 -0400

    re organizing the repo with ts setup

---
+++ src/serverStart.js
@@ -0,0 +1,14 @@
+var http = require('http');
+
+ var server = http.createServer(function(req,res) {
+    res.writeHead(200,{'Content-Type':'text/plain'});
+    res.write('hello');
+    res.end();
+
+});
+
+server.listen(8000);
+
+
+
+console.log('server is running');
