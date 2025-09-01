ab50069ddbbe8dedd3ef82054492f3e12c85a7a8
Nicolas Salcedo <nssalnic@gmail.com>
2025-09-01 16:28:14 -0400

    updated script file

--- src/script.ts
+++ src/script.ts
@@ -1,3 +1,18 @@
  const inputfield = document.getElementById("amount") as HTMLInputElement;
 const button = document.getElementById("generate") as HTMLButtonElement;
  const output = document.getElementsByTagName("output")[0] as HTMLOutputElement;
+
+//assign event types to our event listeners
+button.addEventListener("click",(e:MouseEvent) => {
+    console.log(e.button);
+    buildboxes();
+});
+
+inputfield.addEventListener("keydown",(e:KeyboardEvent) => {
+    console.log(e.key);
+    buildboxes();
+});
+
+function buildboxes(){
+
+}
