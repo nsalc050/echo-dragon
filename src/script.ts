20a5c53d8e24f9ee4a2ba03c2617346c0a5be444
Nicolas Salcedo <nssalnic@gmail.com>
2025-09-01 16:16:34 -0400

    added ts file and new html file

---
+++ src/script.ts
@@ -0,0 +1,3 @@
+ const inputfield = document.getElementById("amount") as HTMLInputElement;
+const button = document.getElementById("generate") as HTMLButtonElement;
+ const output = document.getElementsByTagName("output")[0] as HTMLOutputElement;
